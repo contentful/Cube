d7f13d9913020ed7e0ae8e53a7feaf28b6a68a1f
Boris Bügling <boris@icculus.org>
2015-08-04 14:01:19 +0200

    Swift 2.0 / watchOS 2.0

---
+++ Code/Result.swift
@@ -0,0 +1,12 @@
+//
+//  Result.swift
+//  WatchButton
+//
+//  Created by Boris Bügling on 09/05/15.
+//  Copyright (c) 2015 Boris Bügling. All rights reserved.
+//
+
+import Result
+
+typealias OAuthResult = Result<String, NSError>
+public typealias SphereResult = Result<[String:AnyObject], NSError>
